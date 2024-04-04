d8e892451fb404a124133a441b4abd11ac55a929
LeeYuChan77 <166027482+LeeYuChan77@users.noreply.github.com>
2024-04-04 23:22:38 +0900

    dd

--- calc.py
+++ calc.py
@@ -5,3 +5,33 @@ def add():
 
 
 def divide():
+
+
+def main():
+    print("Let's implement division. Type two numbers for x and y")
+    
+    x = int(input("x > "))
+    y = int(input("y > "))
+    
+    if y==0:
+        print("Error: cannot divide by zero.")
+    else:
+        print("%d / %d = %0.3f" % (x, y, divide(x, y)))
+
+    
+def add(a, b):
+    return a + b
+    
+    
+# TODO: divide() 함수 정의
+        
+def divide(x, y):
+    if y == 0:
+        print("Error: cannot divide by zero.")
+        return
+    else:
+        return round(x / y, 3)
+
+
+if __name__ == "__main__":
+    main()
